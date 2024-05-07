c0074217c6b1befe620da926eaeab8555b351e38
floff02 <2129254@leedstrinity.ac.uk>
2024-05-07 17:41:09 +0100

    Created the initial implementation for the Referral Pathway page

---
+++ app/src/main/java/com/example/hateid/ReferralPathway.java
@@ -0,0 +1,14 @@
+package com.example.hateid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReferralPathway extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_referral_pathway);
+    }
+}
